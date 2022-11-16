4e21e6e5f8455a6216eaa35a3d280502b2966a76
jo su bin <a011096.aivle@gmail.com>
2022-11-17 00:00:34 +0900

    Delete 1037_약수.py

--- 1037_약수.py
+++
@@ -1,7 +0,0 @@
-real_divisor_cnt = int(input())
-real_divisor = list(map(int, input().split()))
-
-real_divisor.sort()
-print(real_divisor[0]*real_divisor[-1])
-
-# print(min(real_divisor)*max(real_divisor)) 
