1330a5a290c3ac49152c8f0ad51e3df7adaac3b7
subin <suvvin97@gmail.com>
2023-02-21 23:32:12 +0900

    23_02_21

---
+++ _silver/18258_큐 2.py
@@ -0,0 +1,23 @@
+import sys
+input=sys.stdin.readline
+N=int(input())
+from collections import deque
+queue=deque([])
+for _ in range(N):
+    command=list(map(str, input().split()))
+    if command[0]=='push':
+        queue.append(command[1])
+    elif command[0]=='pop':
+        if len(queue)==0: print(-1)
+        else: print(queue.popleft())
+    elif command[0]=='size':
+        print(len(queue))
+    elif command[0]=='empty':
+        if len(queue)==0: print(1)
+        else: print(0)
+    elif command[0]=='front':
+        if len(queue)==0: print(-1)  
+        else: print(queue[0]) 
+    elif command[0]=='back':
+        if len(queue)==0: print(-1)                
+        else: print(queue[-1])       
