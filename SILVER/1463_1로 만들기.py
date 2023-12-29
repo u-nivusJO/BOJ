e41ef1047d36d2da31567be5eb8c955e665c6635
subin <suvvin97@gmail.com>
2023-12-29 21:48:38 +0900

    23_12_29

--- SILVER/1463_1로 만들기.py
+++ SILVER/1463_1로 만들기.py
@@ -9,3 +9,25 @@ for i in range(2, N+1):
         result[i]=min(result[i], result[i//3]+1)
 
 print(result[N])
+
+
+# 23_12_29
+N = int(input())
+dp = [0]*1000001
+dp[1] = 1
+dp[2] = 1
+dp[3] = 1
+
+for i in range(4,N+1):
+    if i%6 == 0:
+        dp[i]=min(dp[i//3], dp[i//2], dp[i-1])+1
+    elif i%2 == 0:
+        dp[i]=min(dp[i//2], dp[i-1])+1
+    elif i%3 == 0:
+        dp[i]=min(dp[i//3], dp[i-1])+1
+    else:
+        dp[i]=dp[i-1]+1
+if N==1:
+    print(0)
+else:
+    print(dp[N])
--- SILVER/2839_설탕 배달.py
+++ SILVER/2839_설탕 배달.py
@@ -11,3 +11,22 @@ while N>=0:
     answer+=1
 else:
     print(-1)   
+    
+    
+# 23_12_29
+N = int(input())
+dp = [-1]*5001
+dp[3]=1
+dp[4]=-1
+dp[5]=1
+for i in range(6, N+1):
+    if min(dp[i-3],dp[i-5]) == -1:
+        if max(dp[i-3],dp[i-5]) == -1:
+            dp[i] = -1
+        else:
+            dp[i] = max(dp[i-3],dp[i-5]) + 1
+        
+    else:
+        dp[i] = min(dp[i-3],dp[i-5]) + 1
+
+print(dp[N])
