e51eb4014ea7a5d37ad692b82b3dec1acea88e81
subin <suvvin97@gmail.com>
2023-12-29 21:09:23 +0900

    repo renewal

--- etc/941_A번 - 약수의 합 2.py
+++
@@ -1,7 +0,0 @@
-import sys
-input=sys.stdin.readline
-N=int(input())
-cnt=N
-for n in range(2,N+1):
-    cnt+=n*(N//n)
-print(cnt)            
--- etc/941_C번 - 약.py
+++
@@ -1,8 +0,0 @@
-import sys
-input=sys.stdin.readline
-N=int(input())
-a, b=[2], [1]
-for i in range(1,N):
-    a.append(2*a[i-1]+2*b[i-1])
-    b.append(a[i-1]+b[i-1])
-print(a[N-1])    
