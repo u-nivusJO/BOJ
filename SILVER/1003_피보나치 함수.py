5a99d9fc93b02b76bf193cc597b392d4eaf3dca7
subin <suvvin97@gmail.com>
2023-12-30 15:43:16 +0900

    23_12_30

--- SILVER/1003_피보나치 함수.py
+++ SILVER/1003_피보나치 함수.py
@@ -14,3 +14,16 @@ T=int(input())
 for _ in range(T):
     N=int(input())
     fibonacci(N)
+
+
+# 23_12_30
+dp={i:[0,0] for i in range(41)}
+dp[0]=[1, 0]
+dp[1]=[0, 1]
+for i in range(2,41):
+    dp[i][0], dp[i][1] = dp[i-1][0]+dp[i-2][0], dp[i-1][1]+dp[i-2][1]
+
+T = int(input())
+for t in range(T):
+    N = int(input())
+    print(dp[N][0], dp[N][1])
