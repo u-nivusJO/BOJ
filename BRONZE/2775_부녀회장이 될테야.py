4a64495f37e35670a2d1d38f6ff2805a0b8029f0
subin <suvvin97@gmail.com>
2023-12-31 13:28:40 +0900

    23_12_31

--- BRONZE/2775_부녀회장이 될테야.py
+++ BRONZE/2775_부녀회장이 될테야.py
@@ -13,3 +13,17 @@ for _ in range(T):
         for _ in range(k):
             people=list(accumulate(people))
         print(people[-1])        
+        
+
+# 23_12_31
+dp = [[i for i in range(15)]]+[[0 for i in range(15)] for i in range(15)]
+
+for i in range(1, 15):
+    for j in range(1, 15):
+        for a in range(1, j+1):
+            dp[i][j] += dp[i-1][a]
+T = int(input())
+for t in range(T):
+    k = int(input())
+    n = int(input())
+    print(dp[k][n]) 
