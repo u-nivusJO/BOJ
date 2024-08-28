ff289982a0c43d7d0ebc6145e3d335acfc91b83d
subin <suvvin97@gmail.com>
2024-08-28 23:05:35 +0900

    24_08_28

--- SILVER/15651_N과 M (3).py
+++ SILVER/15651_N과 M (3).py
@@ -5,3 +5,18 @@ for p in list(product(n, repeat=M)):
     for pp in p:
         print(pp, end=' ')
     print( )     
+
+# 백트래킹 1
+N, M = map(int, input().split())
+result = []
+
+def dfs():
+    if len(result) == M:
+        print(*result)
+        return
+    for n in range(1, N+1):
+        result.append(n)
+        dfs()
+        result.pop()
+
+dfs()
--- SILVER/15652_N과 M(4).py
+++ SILVER/15652_N과 M(4).py
@@ -5,3 +5,34 @@ for c in list(combinations_with_replacement(n, M)):
     for cc in c:
         print(cc, end=' ')
     print( )    
+
+# 백트래킹 1
+N, M = map(int, input().split())
+result = []
+
+def dfs(start):
+    if len(result) == M:
+        if sorted(result) == result:
+            print(*result)
+            return
+    for i in range(start, N+1):
+        if sorted(result) == result:
+            result.append(i)
+            dfs(start)
+            result.pop()
+dfs(1)
+
+# 백트래킹 2
+N, M = map(int, input().split())
+result = []
+
+def dfs(start):
+    if len(result) == M:
+        print(*result)
+        return
+    for i in range(start, N+1):
+        result.append(i)
+        dfs(i)
+        result.pop()
+
+dfs(1)
