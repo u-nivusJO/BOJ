9e6c7f6266ec4311b05ef151aeba10d9acce0d65
subin <suvvin97@gmail.com>
2024-08-30 23:12:11 +0900

    24_08_30

--- SILVER/15657_N과 M (8).py
+++ SILVER/15657_N과 M (8).py
@@ -6,3 +6,19 @@ for c in combinations_with_replacement(n ,M):
     for cc in c:
         print(cc, end=' ')
     print( )
+
+# 백트래킹
+N, M = map(int, input().split())
+raw_numbers = list(map(int, input().split()))
+numbers = sorted(raw_numbers)
+result = []
+
+def dfs(start):
+    if len(result) == M:
+        print(*result)
+        return
+    for i in range(start, N):
+        result.append(numbers[i])
+        dfs(i)
+        result.pop()
+dfs(0)
